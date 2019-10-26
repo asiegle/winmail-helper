bd777e60a5a69f49d33c08da72b25fff053f7c9b
Aiden Siegle <aidensiegle@gmail.com>
2019-10-26 01:20:12 -0400

    get message stuff

--- extension/checkAttachment.js
+++ extension/checkAttachment.js
@@ -2,6 +2,19 @@ window.addEventListener ("load", getId, false);
 
 function getId (evt) {
     var messageId = document.getElementsByClassName("adn ads")[0].getAttribute("data-message-id");
+    getMessage('me', messageId, readMessage())
+
     console.log(messageId);
 }
 
+function getMessage(userId, messageId, callback) {
+    var request = gapi.client.gmail.users.messages.get({
+      'userId': userId,
+      'id': messageId
+    });
+    request.execute(callback);
+  }
+
+function readMessage() {
+    console.log(this.id);
+}
