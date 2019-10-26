6664b2c098dc4148d4a6173a1f363e640e0cdc4a
Aiden Siegle <aidensiegle@gmail.com>
2019-10-26 16:14:32 -0400

    substring for msg id

--- extension/checkAttachment.js
+++ extension/checkAttachment.js
@@ -2,6 +2,7 @@ window.addEventListener ("load", getId, false);
 
 function getId (evt) {
     var messageId = document.getElementsByClassName("adn ads")[0].getAttribute("data-message-id");
+    messageId = messageId.substring(1);
     // getMessage('me', messageId, readMessage());
     chrome.runtime.sendMessage({id: messageId}, function(response) {
         console.log(response.worked);
