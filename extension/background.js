28009fd2f90e34b07512086a167cbb8db2e63d8e
Aiden Siegle <aidensiegle@gmail.com>
2019-10-26 00:45:08 -0400

    little clean up

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,1 @@
 //background event handlers
-
-// chrome.webNavigation.onCommitted.addListener(function(e) {
-//     // ...
-//   }, {url: [{hostSuffix: 'google.com'},
-//             {hostSuffix: 'google.com.au'}]});
