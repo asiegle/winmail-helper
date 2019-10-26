0755e62982cd95271b67fa72bd77d93db30834ee
maxkimmelman <max.kimmelman@gmail.com>
2019-10-26 16:15:35 -0400

    added user id detection

--- extension/checkAttachment.js
+++ extension/checkAttachment.js
@@ -3,7 +3,7 @@ window.addEventListener ("load", getId, false);
 function getId (evt) {
     var messageId = document.getElementsByClassName("adn ads")[0].getAttribute("data-message-id");
     var userId = document.getElementsByClassName("gb_hb")[0].innerHTML;
-    // getMessage('me', messageId, readMessage());
+    
     chrome.runtime.sendMessage({msgid: messageId, usrid: userId}, function(response) {
         console.log(response.worked);
       });
