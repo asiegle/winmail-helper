93bcdf7b65a5a2c07a37b83acf55e0ec1165bd07
maxkimmelman <max.kimmelman@gmail.com>
2019-10-26 16:15:17 -0400

    added user id detection

--- extension/background.js
+++ extension/background.js
@@ -68,7 +68,7 @@ function readMessage() {
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        getMessage('me', request.id, readMessage());
+        getMessage(request.usrid, request.msgid, readMessage());
         console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
         sendResponse({worked: "yes"});
     });
--- extension/checkAttachment.js
+++ extension/checkAttachment.js
@@ -2,11 +2,13 @@ window.addEventListener ("load", getId, false);
 
 function getId (evt) {
     var messageId = document.getElementsByClassName("adn ads")[0].getAttribute("data-message-id");
+    var userId = document.getElementsByClassName("gb_hb")[0].innerHTML;
     // getMessage('me', messageId, readMessage());
-    chrome.runtime.sendMessage({id: messageId}, function(response) {
+    chrome.runtime.sendMessage({msgid: messageId, usrid: userId}, function(response) {
         console.log(response.worked);
       });
 
     console.log(messageId);
+    console.log(userId);
 }
 
