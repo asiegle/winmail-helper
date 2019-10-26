6c638f09a8b0d56666986f8742fa0d8dd91f3d4d
Aiden Siegle <aidensiegle@gmail.com>
2019-10-26 01:04:44 -0400

    oath stuff

--- extension/background.js
+++ extension/background.js
@@ -1,1 +1,46 @@
 //background event handlers
+//oauth2 auth
+chrome.identity.getAuthToken(
+	{'interactive': true},
+	function(){
+	  //load Google's javascript client libraries
+		window.gapi_onload = authorize;
+		loadScript('https://apis.google.com/js/client.js');
+	}
+);
+
+function loadScript(url){
+  var request = new XMLHttpRequest();
+
+	request.onreadystatechange = function(){
+		if(request.readyState !== 4) {
+			return;
+		}
+
+		if(request.status !== 200){
+			return;
+		}
+
+    eval(request.responseText);
+	};
+
+	request.open('GET', url);
+	request.send();
+}
+
+function authorize(){
+  gapi.auth.authorize(
+		{
+			client_id: '624743473709-guudjkt9upbm3efr74o563d6bctgl59q.apps.googleusercontent.com',
+			immediate: true,
+			scope: 'https://www.googleapis.com/auth/gmail.readonly'
+		},
+		function(){
+		  gapi.client.load('gmail', 'v1', gmailAPILoaded);
+		}
+	);
+}
+
+function gmailAPILoaded(){
+    console.log("YAY");
+}
