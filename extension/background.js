20c9404d8355998c5c970e0d16a6be238a52734b
Aiden Siegle <aidensiegle@gmail.com>
2019-10-26 20:49:31 -0400

    Getting Messages WORKS

--- extension/background.js
+++ extension/background.js
@@ -17,9 +17,9 @@ chrome.identity.getAuthToken(
 function authorize(){
   gapi.auth.authorize(
 		{
-			client_id: '624743473709-guudjkt9upbm3efr74o563d6bctgl59q.apps.googleusercontent.com',
+			client_id: '7996700904-kte148vl33q9nc1dra384r0rv8hsssua.apps.googleusercontent.com',
 			immediate: true,
-			scope: 'https://mail.google.com/ https://www.googleapis.com/auth/gmail.readonly'
+			scope: 'https://www.googleapis.com/auth/gmail.readonly'
 		},
 		function(){
       gapi.client.load('gmail', 'v1', gmailAPILoaded);
@@ -28,7 +28,8 @@ function authorize(){
 	);
 }
 function gmailAPILoaded(){
-    gapi.client.setApiKey("AIzaSyCVV9GPYJZRiRP3KRuUt6j2riSjZzGqlHw");
+    gapi.client.setApiKey("AIzaSyCZr1Gkvi3Yt-nb0BeYR9yPvd_8S4TtU4Y");
+
     console.log("YAY");
 }
 
@@ -52,19 +53,27 @@ function gmailAPILoaded(){
 //     // request.execute(callback);
 // }
 
-function getMessage2(userId, messageId, token){
-    var url = "https://www.googleapis.com/gmail/v1/users/".concat(encodeURIComponent(userId), "/messages/",encodeURIComponent(messageId),"?format=full&prettyPrint=true&key=AIzaSyCVV9GPYJZRiRP3KRuUt6j2riSjZzGqlHw");
+async function getMessage2(userId, messageId, token){
+    var url = "https://www.googleapis.com/gmail/v1/users/".concat(encodeURIComponent(userId), "/messages/",encodeURIComponent(messageId),"?format=full&prettyPrint=true&key=AIzaSyCZr1Gkvi3Yt-nb0BeYR9yPvd_8S4TtU4Y");
     var auth = "Bearer ".concat(token);
     console.log(url);
-    var response =  fetch(url, {
+    console.log(auth);
+    var response =  await fetch(url, {
         headers: {
-        Accept: "application/json",
-        Authorization: auth
+        Authorization: auth,
+        Accept: "application/json"
       }
     })
 
+    if (response.ok) { // if HTTP-status is 200-299
+      // get the response body (the method explained below)
+      let json = await response.json();
+      console.log(json);
+    } else {
+      alert("HTTP-Error: " + response.status);
+    }
+
     // var myJson =  response.json();
-    console.log(response);
 }
 
 function readMessage() {
@@ -76,7 +85,16 @@ chrome.runtime.onMessage.addListener(
         console.log(request.usrid);
         console.log(request.msgid);
         chrome.identity.getAuthToken(function(token){
+          // var test = await fetch("https://www.googleapis.com/gmail/v1/users/asiegle%40u.rochester.edu/messages/msg-f%3A1645486631346554900?format=full&prettyPrint=true&key=AIzaSyCZr1Gkvi3Yt-nb0BeYR9yPvd_8S4TtU4Y", {
+          //     headers: {
+          //     Accept: "application/json",
+          //     Authorization: "Bearer ya29.Il-pBz0vD6BorqnXcXQXj78_FEMUn5xt5Y8FR2000FrDl71266uN155sifgn0PbcvUDPTLhNyTF4o8vkRGQpPSlpGliCDukVkE2lOwMVqDDJHP0xZODrajP6XJzjkiOthw"
+          //   }
+          // })
+          // console.log(test)
           getMessage2(request.usrid, request.msgid, token);
+          // getMessage(request.usrid, request.msgid, readMessage);
+          
         })
         console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
         sendResponse({worked: "yes"});
